4e5ad39bbf2669fc0c4516f659dd5ac8d4d1b643
sunbin <sunbin@windows10.microdone.cn>
2019-05-23 21:37:45 +0800

    加入spring boot admin

--- src/main/java/com/sunbin/springboot/SpringbootApplication.java
+++ src/main/java/com/sunbin/springboot/SpringbootApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+
 @SpringBootApplication
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60)
 @EnableScheduling
+@EnableAdminServer
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
